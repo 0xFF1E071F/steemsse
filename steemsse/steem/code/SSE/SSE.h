d16ab752d767f4a9711e8858469b3a7e82c523b6
steven-seagal <steven-seagal@mycompany.com>
2013-07-11 07:42:18 +0000

    structure diskman
    git-svn-id: svn://svn.code.sf.net/p/steemsse/code/trunk@53 dd3fbd3b-ef5d-4b78-98e8-da144fa02e5b

--- steemsse/steem/code/SSE/SSE.h
+++ steemsse/steem/code/SSE/SSE.h
@@ -890,6 +890,7 @@ problem, multiple struct/class definition not allowed?
 #define SS_STRUCTURE_DATALOADSAVE_H
 #define SS_STRUCTURE_DEBUGEMU_H
 #define SS_STRUCTURE_DIRID_H
+#define SS_STRUCTURE_DISKMAN_H
 //...
 #endif
 
