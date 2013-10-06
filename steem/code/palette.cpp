2347ba1709c476e0b33cda34a5ede7830e88a935
steven-seagal <steven-seagal@mycompany.com>
2013-10-06 08:23:10 +0000

    structure palette.h
    git-svn-id: svn://svn.code.sf.net/p/steemsse/code/trunk/steemsse@152 dd3fbd3b-ef5d-4b78-98e8-da144fa02e5b

--- steem/code/palette.cpp
+++ steem/code/palette.cpp
@@ -9,6 +9,36 @@ to create the ST palette and also the code to add to the PC palette.
 #pragma message("Included for compilation: palette.cpp")
 #endif
 
+#if defined(STEVEN_SEAGAL) && defined(SS_STRUCTURE_PALETTE_H)
+
+#define EXT
+EXT int brightness=0,contrast=0;
+int palhalf=0,palnum=0;
+EXT bool palette_changed=false;
+#ifdef WIN32
+HPALETTE winpal=NULL,oldwinpal;
+HDC PalDC;
+#elif defined(UNIX)
+const long standard_palette[18][2]={
+	{0,0},
+	{8,0xff0000},{9,0x00ff00},{10,0x0000ff},
+	{11,0xffff00},{12,0x00ffff},{13,0xff00ff},
+	{13,0xc0c0c0},
+	{14,0xe0e0e0},
+	{247,0x0000c0},
+	{248,0x800000},{249,0x008000},{250,0x000080},
+	{251,0x808000},{252,0x008080},{253,0x800080},
+	{254,0x808080},
+	{255,0xffffff}};
+Colormap colormap=0;
+XColor new_pal[257];
+#endif
+long logpal[257];
+
+#undef EXT
+
+#endif
+
 //---------------------------------------------------------------------------
 void make_palette_table(int brightness,int contrast)
 {
---
+++ steem/code/palette.decla.h
@@ -0,0 +1,38 @@
+#pragma once
+#ifndef PALETTE_DECLA_H
+#define PALETTE_DECLA_H
+
+#define EXT extern
+
+EXT void palette_copy();
+EXT void palette_flip();
+EXT WORD palette_add_entry(DWORD col);
+EXT void palette_convert_all();
+EXT void palette_convert(int);
+
+//#ifdef IN_MAIN
+EXT void palette_prepare(bool);
+EXT void palette_remove();
+EXT bool palette_changed;
+
+EXT void make_palette_table(int brightness,int contrast);
+
+EXT int brightness,contrast;
+EXT int palhalf,palnum;
+
+#ifdef WIN32
+EXT HPALETTE winpal,oldwinpal;
+EXT HDC PalDC;
+#elif defined(UNIX)
+EXT const long standard_palette[18][2];
+EXT Colormap colormap;
+EXT XColor new_pal[257];
+
+#endif
+
+EXT long logpal[257];
+//#endif
+
+#undef EXT
+
+#endif//PALETTE_DECLA_H
