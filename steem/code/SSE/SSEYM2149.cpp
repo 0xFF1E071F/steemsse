a70cd8f3485606d3eeb8736a4cca92953c0f0892
steven-seagal <steven-seagal@mycompany.com>
2014-11-11 09:16:36 +0000

    ym2149_fixed_vol as apart data file: SSEYM2149
    git-svn-id: svn://svn.code.sf.net/p/steemsse/code/trunk/steemsse@297 dd3fbd3b-ef5d-4b78-98e8-da144fa02e5b

--- steem/code/SSE/SSEYM2149.cpp
+++ steem/code/SSE/SSEYM2149.cpp
@@ -3,15 +3,78 @@
 #if defined(STEVEN_SEAGAL)
 
 #if defined(SSE_YM2149)
+/*  In v3.5.1, object YM2149 was only used for drive management.
+    In v3.7.0, sound functions were introduced.
+*/
 
 #include "../pch.h"
 #include <cpu.decla.h>
 #include <iorw.decla.h>
 #include <psg.decla.h>
 #include "SSEYM2149.h"
+#include "SSEOption.h"
+extern EasyStr GetEXEDir();//TODO
 
-/*  In v3.5.1, object YM2149 is only used for drive management.
-*/
+
+TYM2149::TYM2149() { //v3.7.0
+#if defined(SSE_YM2149_DYNAMIC_TABLE)//v3.7.0
+  p_fixed_vol_3voices=NULL;
+#endif
+}
+
+
+TYM2149::~TYM2149() { //v3.7.0
+#if defined(SSE_YM2149_DYNAMIC_TABLE)//v3.7.0
+  FreeFixedVolTable();
+#endif
+}
+
+
+//SOUND
+
+
+#if defined(SSE_YM2149_DYNAMIC_TABLE)//v3.7.0
+
+
+void TYM2149::FreeFixedVolTable() {
+  if(p_fixed_vol_3voices)
+  {
+    TRACE("free memory of PSG table %p\n",p_fixed_vol_3voices);
+    delete [] p_fixed_vol_3voices;
+    p_fixed_vol_3voices=NULL;
+  }
+}
+
+
+bool TYM2149::LoadFixedVolTable() {
+  bool ok=false;
+  ASSERT(!p_fixed_vol_3voices); // may happen...
+  FreeFixedVolTable(); //safety
+  p_fixed_vol_3voices=new WORD[16*16*16];
+  ASSERT(p_fixed_vol_3voices);
+  EasyStr filename=GetEXEDir();
+  filename+=YM2149_FIXED_VOL_FILENAME;
+  FILE *fp=fopen(filename.Text,"r+b");
+  if(fp && p_fixed_vol_3voices)
+  {
+    int nwords=fread(p_fixed_vol_3voices,sizeof(WORD),16*16*16,fp);
+    if(nwords==16*16*16)
+      ok=true;
+    fclose(fp);
+//    TRACE("PSG %s loaded %d words in ram %p\n",filename.Text,nwords,p_fixed_vol_3voices);
+  }
+  else
+  {
+    TRACE("No file %s\n",filename.Text);
+    FreeFixedVolTable();
+    SSE_OPTION_PSG=0;
+  }
+  return ok;
+}
+
+#endif//SSE_YM2149_DYNAMIC_TABLE
+
+//DRIVE
 
 BYTE TYM2149::Drive(){
   BYTE drive=NO_VALID_DRIVE; // different from floppy_current_drive()
