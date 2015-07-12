61afe230dba2735d480fdaa5ce5f6845d9e3182b
steven-seagal <steven-seagal@mycompany.com>
2015-07-12 08:10:01 +0000

    last changes for v3.7.2 unix
    git-svn-id: svn://svn.code.sf.net/p/steemsse/code/trunk/steemsse@394 dd3fbd3b-ef5d-4b78-98e8-da144fa02e5b

--- steem/code/x/x_diskman.cpp
+++ steem/code/x/x_diskman.cpp
@@ -695,7 +695,9 @@ void TDiskManager::Show()
 #if defined(SSE_DISK_SCP)
   dir_lv.ext_sl.Add(4,"scp",ICO16_DISK,ICO16_DISKLINK,ICO16_DISKLINKBROKEN,ICO16_DISK_RO);
 #endif
-
+#if defined(SSE_DISK_HFE)
+  dir_lv.ext_sl.Add(4,"hfe",ICO16_DISK,ICO16_DISKLINK,ICO16_DISKLINKBROKEN,ICO16_DISK_RO);
+#endif
   ArchiveTypeIdx=dir_lv.ext_sl.NumStrings;
   int zipicon=ICO16_ZIP_RO;
   if (FloppyArchiveIsReadWrite) zipicon=ICO16_ZIP_RW;
