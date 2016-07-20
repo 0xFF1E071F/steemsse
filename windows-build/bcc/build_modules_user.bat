9073b59beb71b44e105ac78bc69e340f7677d395
steven-seagal <steven-seagal@mycompany.com>
2016-07-20 20:12:17 +0000

    Compiler configs clean-up
    git-svn-id: svn://svn.code.sf.net/p/steemsse/code/trunk/steemsse@495 dd3fbd3b-ef5d-4b78-98e8-da144fa02e5b

--- windows-build/bcc/build_modules_user.bat
+++ windows-build/bcc/build_modules_user.bat
@@ -5,15 +5,15 @@ echo Build script for Steem SSE - BCC build
 rem local:
 
 cd c:\data\prg\st\windows-build\bcc
-set NASMPATH=D:\Console\nasm\
-set BCCROOT=D:\Console\bcc\
-set BCCPATH=D:\Console\bcc\bin\
-set COPYPATH=G:\emu\st\bin\steem\
-rem set ROOT=c:\data\prg\st\
-set ROOT=G:\Downloads\steemsse-code\steemsse\
+set NASMPATH=c:\Console\nasm\
+set BCCROOT=c:\Console\bcc\
+set BCCPATH=c:\Console\bcc\bin\
+set COPYPATH=d:\emu\st\bin\steem\
+set ROOT=c:\data\prg\st\
+//set ROOT=G:\Downloads\steemsse-code\steemsse\
 rem the rest should be the same for all systems:
 
-set PROGRAMNAME=SteemBeta BCC.exe
+set PROGRAMNAME=Steem 3.2+ BCC.exe
 rem manually create obj directory if necessary
 set OUT=obj
 del "%OUT%\*.exe"
@@ -31,16 +31,10 @@ if NOT EXIST obj\asm_portio.obj (
 @echo ON
 "%NASMPATH%nasm" -o obj\asm_portio.obj -fobj -dWIN32 %ROOT%\include\asm\asm_portio.asm
 )
-@echo OFF
 
-echo -----------------------------------------------
-echo Building 3rd party code using Borland C/C++ 5.5
-echo -----------------------------------------------
-"%BCCPATH%make.exe" -fmakefile_modules.txt -DDONT_ALLOW_DEBUG -DSTEVEN_SEAGAL 3rdparty
-echo ------------------------------------------
-echo Building Steem SSE using Borland C/C++ 5.5
-echo ------------------------------------------
-"%BCCPATH%make.exe" -fmakefile_modules.txt -B -DDONT_ALLOW_DEBUG -DBCC_BUILD -DSTEVEN_SEAGAL
+echo on
+
+"%BCCPATH%make.exe" -fmakefil2.txt -B -DDONT_ALLOW_DEBUG -DBCC_BUILD 
 
 ren "%OUT%\Steem.exe" "%PROGRAMNAME%"
 copy "%OUT%\%PROGRAMNAME%" "%COPYPATH%"
