7fb8ccc0ff9c3b05d70a56ab7353279e56fc38d2
steven-seagal <steven-seagal@mycompany.com>
2017-05-05 08:21:06 +0000

    compiler settings LE builds
    git-svn-id: svn://svn.code.sf.net/p/steemsse/code/trunk/steemsse@600 dd3fbd3b-ef5d-4b78-98e8-da144fa02e5b

---
+++ windows-build/bcc/build_LE_debug.bat
@@ -0,0 +1,34 @@
+@echo off
+
+echo Build script for Steem LE - BCC build with boiler (unreleased)
+
+call build_bcc_set.bat
+
+set PROGRAMNAME=BoilerBeta BCC.exe
+
+del "%OUT%\6301.obj"
+del "%OUT%\AviFile.obj"
+del "%OUT%\dsp.obj"
+del "%OUT%\*.exe"
+
+rem call build_bcc_asm.bat
+
+echo -----------------------------------------------
+echo Building 3rd party code using Borland C/C++ 5.5
+echo -----------------------------------------------
+"%BCCPATH%make.exe" -fmake_LE.txt -DFORCE_DEBUG_BUILD 3rdparty -DSTEVEN_SEAGAL
+rem pause
+echo ---------------------------------------------------
+echo Building Steem SSE (Boiler) using Borland C/C++ 5.5
+echo ---------------------------------------------------
+
+"%BCCPATH%make.exe" -fmake_LE.txt -DFORCE_DEBUG_BUILD -DBCC_BUILD -DSTEVEN_SEAGAL
+
+rem "%BCCPATH%make.exe" -fmakefile.txt -DFORCE_DEBUG_BUILD -DBCC_BUILD link  
+
+
+if exist "%OUT%\Steem.exe" (ren "%OUT%\Steem.exe" "%PROGRAMNAME%"
+copy "%OUT%\%PROGRAMNAME%" "%COPYPATH%")
+
+pause
+
