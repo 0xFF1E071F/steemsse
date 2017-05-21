2db06551e9dbc641031dcedcfe5099f316f206d3
steven-seagal <steven-seagal@mycompany.com>
2017-05-21 10:21:03 +0000

    LE scripts D3D
    git-svn-id: svn://svn.code.sf.net/p/steemsse/code/trunk/steemsse@605 dd3fbd3b-ef5d-4b78-98e8-da144fa02e5b

---
+++ windows-build/bcc/build_LED3D_user.bat
@@ -0,0 +1,31 @@
+@echo off
+
+echo Build script for Steem LE - BCC build
+
+call build_bcc_set.bat
+
+set PROGRAMNAME=Beta BCC LE D3D.exe
+
+del "%OUT%\6301.obj"
+del "%OUT%\AviFile.obj"
+del "%OUT%\dsp.obj"
+del "%OUT%\*.exe"
+
+rem call build_bcc_asm.bat
+
+echo -----------------------------------------------
+echo Building 3rd party code using Borland C/C++ 5.5
+echo -----------------------------------------------
+"%BCCPATH%make.exe" -fmake_LE.txt -DDONT_ALLOW_DEBUG -DSTEVEN_SEAGAL  -DSSE_D3D 3rdparty
+rem "%BCCPATH%make.exe" -fmake_LE.txt -DDONT_ALLOW_DEBUG -DSTEVEN_SEAGAL 3rdparty >build_user_3rdparty.log
+echo ------------------------------------------
+echo Building Steem SSE using Borland C/C++ 5.5
+echo ------------------------------------------
+"%BCCPATH%make.exe" -fmake_LE.txt -DDONT_ALLOW_DEBUG -DBCC_BUILD -DSTEVEN_SEAGAL  -DSSE_D3D
+rem "%BCCPATH%make.exe" -fmake_LE.txt -DDONT_ALLOW_DEBUG -DBCC_BUILD -DSTEVEN_SEAGAL >build_user_steem.log
+
+if exist "%OUT%\Steem.exe" (ren "%OUT%\Steem.exe" "%PROGRAMNAME%"
+copy "%OUT%\%PROGRAMNAME%" "%COPYPATH%")
+
+
+pause
